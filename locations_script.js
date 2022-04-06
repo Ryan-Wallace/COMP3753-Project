4cb0ba395276895b7467251bc74072780b00fe84
bbnutt07 <bbnutt07@gmail.com>
2022-04-06 16:05:18 -0300

    finished locations

--- locations_script.js
+++ locations_script.js
@@ -7,38 +7,17 @@ $(document).ready(function() {
 });
 
 // get learnable moves for a pokemon
-function getLearns(pokemon) {
-    var api_val = "/api/learns/" + pokemon;
+function getPokemonHere(location) {
+    var api_val = "/api/pokemon/" + location;
     var html = "";
 
     $.ajax({
         url: api_val,
         success: function(data)  {
-            html = "<table class=\"table\"><tr><th>Move</th><th>Lvl</th></tr>";
+            html = "<table class=\"table\"><tr><th>Pokemon</th><th>Terrain</th><th>Rate</th><th>Lvl</th></tr>";
 
             $(data).each(function(key, object) {
-                html += "<tr><td>" + object['Move'] + "</td><td>" + object['Lvl'] + "</td></tr>";
-            });
-        
-            html += "</table>";
-        },
-        async: false
-    });
-    return html;
-}
-
-// get possible locations for a pokemon
-function getFound(pokemon) {
-    var api_val = "/api/found/" + pokemon;
-    var html = "";
-
-    $.ajax({
-        url: api_val,
-        success: function(data)  {
-            html = "<table class=\"table\"><tr><th>Location</th><th>Terrain</th><th>Catch rate%</th><th>Lvl</th></tr>";
-
-            $(data).each(function(key, object) {
-                html += "<tr><td>" + object['Location'] + "</td><td>" + object['Terrain'] + "</td><td>" + object['Rate'] + "</td><td>" + object['Lvl'] + "</td></tr>";
+                html += "<tr><td>" + object['Pokemon'] + "</td><td>" + object['Terrain'] + "</td><td>" + object['Rate'] + "</td><td>" + object['Lvl'] + "</td></tr>";
             });
         
             html += "</table>";
@@ -60,17 +39,12 @@ function getLocations() {
         $(data).each(function(key, object) {
             // HTML table row
             table_rows += "<tr><td>" + object['Name'] + "</td><td>" + object['Region'] + "</td><td>" + object['Climate'] + "</td>";
-            table_rows += "<td><button class=\"btn btn-primary\" type=\"button\" data-bs-toggle=\"collapse\" data-bs-target=\"#" + object['Name'] + "\">Info</button></td></tr>";
-            table_rows += "<tr class=\"collapse\" id=\"" + object['Name'] + "\"><td colspan=\"10\"><div class=\"row\">";
-            table_rows += "<div class=\"col-8\"><div class=\"card\"><div class=\"card-header\">Found:</div><div class=\"card-body\">";
+            table_rows += "<td><button class=\"btn btn-primary\" type=\"button\" data-bs-toggle=\"collapse\" data-bs-target=\"#" + object['Name'].replace(/\s/g,'') + "\">Info</button></td></tr>";
+            table_rows += "<tr class=\"collapse\" id=\"" + object['Name'].replace(/\s/g,'') + "\"><td colspan=\"10\"><div class=\"row\">";
+            table_rows += "<div class=\"col-12\"><div class=\"card\"><div class=\"card-header\">Found:</div><div class=\"card-body\">";
             
             // get location data
-            table_rows += getFound(object['Name']);
-
-            table_rows += "</div></div></div><div class=\"col-4\"><div class=\"card\"><div class=\"card-header\">Learns:</div><div class=\"card-body\">";
-            
-            // get learnable moves data
-            table_rows += getLearns(object['Name']);
+            table_rows += getPokemonHere(object['Name']);
 
             table_rows += "</div></div></div></div></td></tr>";
         });
