306edad72e618329fe785fdd780e3eb79d38dbeb
bbnutt07 <bbnutt07@gmail.com>
2022-04-11 21:56:00 -0300

    Update server.js

--- server.js
+++ server.js
@@ -139,7 +139,6 @@ function reconnect() {
         }
         connection.on('error', err => {
             if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-                console.log("reconnecting...");
                 reconnect();
             } else {
                 throw err;
@@ -157,7 +156,6 @@ app.listen(8080, () => {
         }
         connection.on('error', err => {
             if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-                console.log("reconnecting...");
                 reconnect();
             } else {
                 throw err;
